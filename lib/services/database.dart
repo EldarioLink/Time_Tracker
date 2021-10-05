058452669a641ece9274875bbb12b9d5010b4f28
ElWork <myEmail@example.com>
2021-10-05 13:55:07 +0300

    feat: add cloud firestore

---
+++ lib/services/database.dart
@@ -0,0 +1,22 @@
+import 'package:time_tracker/app/home/models/job.dart';
+import 'package:time_tracker/services/api_path.dart';
+import 'package:time_tracker/services/firestore_services.dart';
+
+abstract class Database {
+  Future<void> createJob(Job job);
+  Stream<List<Job>> jobsStream();
+}
+
+class FirestoreDatabase implements Database {
+  FirestoreDatabase({required this.uid});
+  final String uid;
+
+  final _service = FirestoreService.instance;
+  Future<void> createJob(Job job) => _service.setData(
+        path: APIPath.job(uid, 'job_abc'),
+        data: job.toMap(),
+      );
+
+  Stream<List<Job>> jobsStream() => _service.collectionStream(
+      path: APIPath.jobs(uid), builder: (data) => Job.fromMap(data));
+}
