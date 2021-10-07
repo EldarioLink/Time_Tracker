c2f789b0cc922b15185036d9232089f2f8f9866e
ElWork <myEmail@example.com>
2021-10-07 15:36:18 +0300

    :ref

--- lib/services/firestore_services.dart
+++ lib/services/firestore_services.dart
@@ -10,6 +10,11 @@ class FirestoreService {
     await reference.set(data);
   }
 
+  Future<void> deleteData({required String path}) async {
+    final reference = FirebaseFirestore.instance.doc(path);
+    await reference.delete();
+  }
+
   Stream<List<T>> collectionStream<T>({
     required String path,
     required T Function(Map<String, dynamic> data, String documentId) builder,
