ee1ade313de090de39924af6d44e0bd4826d5396
ElWork <myEmail@example.com>
2021-10-14 08:12:40 +0300

    feat: adding currentUser

--- lib/app/sign_in/email_sign_in_bloc.dart
+++ lib/app/sign_in/email_sign_in_bloc.dart
@@ -1,18 +1,21 @@
 import 'dart:async';
 
+import 'package:rxdart/rxdart.dart';
 import 'package:time_tracker/app/sign_in/email_sign_in_model.dart';
 import 'package:time_tracker/services/auth.dart';
 
 class EmailSignInBloc {
   EmailSignInBloc({required this.auth});
   final AuthBase auth;
-  final StreamController<EmailSignInModel> _modelController =
-      StreamController<EmailSignInModel>();
 
-  Stream<EmailSignInModel> get modelStream => _modelController.stream;
-  EmailSignInModel _model = EmailSignInModel();
+  final _modelSubject =
+      BehaviorSubject<EmailSignInModel>.seeded(EmailSignInModel());
+
+  Stream<EmailSignInModel> get modelStream => _modelSubject.stream;
+  EmailSignInModel get _model => _modelSubject.value;
+
   void dispose() {
-    _modelController.close();
+    _modelSubject.close();
   }
 
   Future<void> submit() async {
@@ -56,14 +59,12 @@ class EmailSignInBloc {
     bool? isLoading,
     bool? submitted,
   }) {
-    _model = _model.copyWith(
+    _modelSubject.value = _model.copyWith(
       email: email,
       password: password,
       formType: formType,
       isLoading: isLoading,
       submitted: submitted,
     );
-
-    _modelController.add(_model);
   }
 }
