bcfddf3cbf003e8e0f908d0687dcf724d5b752f9
ElWork <myEmail@example.com>
2021-10-18 09:40:28 +0300

    feat: complete app with rx dart feature

--- lib/app/sign_in/email_sign_in_bloc.dart
+++ lib/app/sign_in/email_sign_in_bloc.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
-
 import 'package:rxdart/rxdart.dart';
+
 import 'package:time_tracker/app/sign_in/email_sign_in_model.dart';
 import 'package:time_tracker/services/auth.dart';
 
